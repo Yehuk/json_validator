c35105af47e3202bf76c74ef138e8986125f4bc7
Yehuk <nikita.gorlyak@yandex.ru>
2018-01-15 14:29:54 +0300

    #3

--- src/main/java/json_validate.java
+++ src/main/java/json_validate.java
@@ -10,7 +10,13 @@ import java.net.InetSocketAddress;
  * Class to implement server and validate json files
  */
 public class json_validate {
+        /**
+         *
+         */
         private static final int PORT = 80;     //We listen this port
+        /**
+         *
+         */
         private static final int CODE = 200;    //Reply code
 
         /**
@@ -20,41 +26,90 @@ public class json_validate {
          */
 
         public json_validate(HttpServer server) throws IOException {
-                Gson gson = new GsonBuilder().setPrettyPrinting().create();        //We create GsonBuilder and
-                // configure it to output Json that fits in a page for pretty printing
-                server.bind(new InetSocketAddress(PORT), 0);    //Binding server to PORT
-                server.createContext("/", httpExchange -> {     //Start of the server's work
-                        int id = 0;     //Counter of request-id
+                /**
+                 * We create GsonBuilder and configure it to output Json that fits in a page for pretty printing
+                 */
+                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                /**
+                 * Binding server to PORT
+                 */
+                server.bind(new InetSocketAddress(PORT), 0);
+                /**
+                 * Start of the server's work
+                 */
+                server.createContext("/", httpExchange -> {
+                        /**
+                         * Counter of request-id
+                         */
+                        int id = 0;
+                        /**
+                         * We use BufferedReader to read text from a character-input stream, buffering characters
+                         * so as to provide for the efficient reading of characters, arrays, and lines. And we are using InputStreamReader
+                         * as a bridge between byte streams and character streams:  It reads bytes and decodes them into characters using a specified charset.
+                         * Parameter of InputStreamReader is InputStream, which we get from method getRequestBody, which
+                         * returns a InputStream for reading the request body.
+                         */
                         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpExchange.getRequestBody()));
-                        //We use BufferedReader to read text from a character-input stream, buffering characters
-                        //so as to provide for the efficient reading of characters, arrays, and lines. And we are using InputStreamReader
-                        //as a bridge between byte streams and character streams:  It reads bytes and decodes them into characters using a specified charset.
-                        //Parameter of InputStreamReader is InputStream, which we get from method getRequestBody, which
-                        //returns a InputStream for reading the request body.
-                        String buildString = bufferedReader.readLine();         //Buffer line
-                        StringBuilder stringBuilder = new StringBuilder();      //Here we read until EOF and building
-                        // a line, containing our request
+                        /**
+                         * Buffer line
+                         */
+                        String buildString = bufferedReader.readLine();
+                        /**
+                         * Here we read until EOF and building a line, containing our request
+                         */
+                        StringBuilder stringBuilder = new StringBuilder();
                         while (buildString != null) {
                                 stringBuilder.append(buildString);
                                 buildString = bufferedReader.readLine();
                         }
                         String request = stringBuilder.toString();
-
-                        System.out.println("Request: " + request);      //Shows the request
-                        String response = null;                         //Response is empty at the start
-
-                        try {   //Here we try to convert json to string and back and wait for exceptions
-                                Object object = gson.fromJson(request, Object.class);   //Json to string
-                                response = gson.toJson(object);                         //String to json
-                        } catch (JsonSyntaxException exception) {                       //If there is an exception
-                                JsonObject error = new JsonObject();                                    //Creating
-                                //the json foundation to the response
-                                String errorDescription = exception.getMessage().split(": ")[1];      //"Extract"
-                                //part of the error description before ": "
-                                String errorMessage = errorDescription.split(" at ")[0];        //This part is Message
-                                String errorPlace = errorDescription.split(" at ")[1];          //And this one is Place
-                                int errorCode = exception.hashCode();   //Error's hash
-                                //And now we form beautiful view for the response
+                        /**
+                         * Shows the request
+                         */
+                        System.out.println("Request: " + request);
+                        /**
+                         * Response is empty at the start
+                         */
+                        String response = null;
+                        /**
+                         * Here we try to convert json to string and back and wait for exceptions
+                         */
+                        try {
+                                /**
+                                 * Json to string
+                                 */
+                                Object object = gson.fromJson(request, Object.class);
+                                /**
+                                 * String to json
+                                 */
+                                response = gson.toJson(object);
+                                /**
+                                 * If there is an exception
+                                 */
+                        } catch (JsonSyntaxException exception) {
+                                /**
+                                 * Creating the json foundation to the response
+                                 */
+                                JsonObject error = new JsonObject();
+                                /**
+                                 * "Extract" part of the error description before ": "
+                                 */
+                                String errorDescription = exception.getMessage().split(": ")[1];
+                                /**
+                                 * This part is Message
+                                 */
+                                String errorMessage = errorDescription.split(" at ")[0];
+                                /**
+                                 * And this one is Place
+                                 */
+                                String errorPlace = errorDescription.split(" at ")[1];
+                                /**
+                                 * Error's hash
+                                 */
+                                int errorCode = exception.hashCode();
+                                /**
+                                 * And now we form beautiful view for the response
+                                 */
                                 error.addProperty("errorCode", errorCode);
                                 error.addProperty("errorMessage", errorMessage);
                                 error.addProperty("errorPlace", errorPlace);
@@ -63,9 +118,13 @@ public class json_validate {
                                 response = gson.toJson(error);
                         }
                         id = id + 1;
-                        System.out.println("Response: " + response);     //Shows the response
-
-                        //Sending the response and closing
+                        /**
+                         * Shows the response
+                         */
+                        System.out.println("Response: " + response);
+                        /**
+                         * Sending the response and closing
+                         */
                         httpExchange.sendResponseHeaders(CODE, response.length());
                         httpExchange.getResponseBody().write(response.getBytes());
                         httpExchange.close();
@@ -81,9 +140,15 @@ public class json_validate {
          * @throws IOException
          */
         public static void main(String[] args) throws IOException {
-                final HttpServer server = HttpServer.create();  //We create server
+                /**
+                 * We create server
+                 */
+                final HttpServer server = HttpServer.create();
                 json_validate json = new json_validate(server);
-                json.start(server);                             //We launch server
+                /**
+                 * We launch server
+                 */
+                json.start(server);
         }
 
         /**
